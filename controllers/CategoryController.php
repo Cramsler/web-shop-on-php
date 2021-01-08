c0a4fd162bf917a46408d5f1fed3a8640a02fb35
Cramsler <70647061+Cramsler@users.noreply.github.com>
2021-01-08 17:32:12 +0400

    *

---
+++ controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class CategoryController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
---
+++ models/Good.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Good extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'good';
+    }
+}
