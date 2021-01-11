945e7272c966c80f01cc63e4bea6533f5318ab00
Cramsler <70647061+Cramsler@users.noreply.github.com>
2021-01-11 20:09:44 +0400

    *

---
+++ controllers/CartController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace app\controllers;
+
+use app\models\Cart;
+use Yii;
+use app\models\Good;
+use yii\web\Controller;
+
+class CartController extends Controller
+{
+    public function actionCart($name)
+    {
+        $good = new Good();
+        $good = $good->getOneGood($name);
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->addToCart($good);
+        return $this->renderPartial('cart', compact('good', 'session'));
+    }
+
+}
--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,22 @@
 $('.cart').on('click', function () {
     $('#cart').modal('show');
 });
+
+$('.product-button__add').on('click', function (event) {
+    event.preventDefault();
+    let name = $(this).data('name');
+    console.log(name);
+
+    $.ajax({
+        url: 'cart/cart',
+        data: {name: name},
+        type: 'GET',
+
+        success: function (res){
+            $('#cart .modal-content').html(res);
+        },
+        error: function () {
+            alert('ошибка');
+        }
+    })
+})
